ae036b4e0a55ce56ce04aed1e622094bcd39e1f3
bob <robert.matthew.brooks@gmail.com>
2023-07-11 22:13:34 +0100

    render image on deploy

--- react-data-fetching/src/components/Header.jsx
+++ react-data-fetching/src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <header>
-            <img src="./chimney.png" />
+            <img src="https://raw.githubusercontent.com/robert-matthew-brooks/carbon-intensity-data-front-end/main/react-data-fetching/chimney.png" />
             <h1>Carbon Intensity Data</h1>
             <h3>from electricity generation in Great Britain</h3>
         </header>
