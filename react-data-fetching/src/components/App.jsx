8bd8aeaceaba35b15e556af181cc0750d67e5dba
bob <robert.matthew.brooks@gmail.com>
2023-07-11 23:20:02 +0100

    add readme, refactor

--- react-data-fetching/src/components/App.jsx
+++ react-data-fetching/src/components/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 import Menu from './Menu.jsx';
 import Header from './Header.jsx';
 import Text from './Text.jsx';
@@ -12,8 +10,8 @@ function App() {
     id: 'about',
     title: 'About',
     paras: [
-      'This page is designed as a front-end for a public back-end api. The project is focussed around presenting the data using <em>react</em> components. These components are reusable, and they also allow the displayed information can change based on user input. Another consideration for this project was using a clear layout with landmark tags that can be easily interpreted by accessibility tools.',
-      'The react module for displaying the charts is <a href="https://react-chartjs-2.js.org/">react-chartjs-2</a>. The api can be found at <a href="https://api.carbonintensity.org.uk/">api.carbonintensity.org.uk</a>, and it provides data about carbon intensity levels in Great Britain as a result of electricity generation.'
+      'This page is a front end for a public back end api. The project was designed process and present the raw data using <em>React</em> components. These components are reusable, and they also allow the displayed information can change based on user input. Another consideration for this project was using a clear layout with landmark tags that can be easily interpreted by accessibility tools.',
+      'The <a href="https://api.carbonintensity.org.uk/">back end api</a> provides data about carbon intensity levels in Great Britain as a result of electricity generation. The components below filter the data for specific time periods, and render a graph using a <a href="https://react-chartjs-2.js.org/">third party library</a>.'
     ]
   };
 
@@ -23,7 +21,17 @@ function App() {
     paras: [
       'The bar chart is responsive, and the relevent data will be fetched from the api on demand. When the data fetching is slow, a loading message will show over the chart. This can be simulated on Google Chrome within <em>Developer Options</em>, by selecting <em>Slow 3G</em> under the <em>Network</em> tab',
       'The page layout is also responsive, and will reduce wasted space when the display area is smaller. An option to simulate mobile browsers screen types can also be found within <em>Developer Options</em>.',
-      'The pie chart below is static, and will not reload with other react components.'
+      'The pie chart below is static and will not reload with other React components. Seven requests are made to the api when the page loads (one for each day), then the data is processed and combined into a chart.'
+    ]
+  };
+
+  const planning = {
+    id: 'planning',
+    title: 'Planning',
+    paras: [
+      'While planning the app, I considered the heirarchy of the components that would be used. The initial <em>wireframe</em> and <em>component tree</em> diagrams can see below. These were helpful for not only nesting html tags in a logical manner, but also to ensure React state variables were held in the correct level of the tree and only passed down when needed.',
+      '<img src="./src/img/wireframe.png" alt="Wireframe plan of webpage">',
+      '<img src="./src/img/component-tree.png" alt="Component tree diagram of webpage">'
     ]
   };
 
@@ -37,6 +45,7 @@ function App() {
         <DayChart />
         <Text id={moreInfo.id} title={moreInfo.title} paras={moreInfo.paras} />
         <WeekChart />
+        <Text id={planning.id} title={planning.title} paras={planning.paras} />
       </main>
 
       <Footer />
--- react-data-fetching/src/components/Header.jsx
+++ react-data-fetching/src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <header>
-            <img src="https://raw.githubusercontent.com/robert-matthew-brooks/carbon-intensity-data-front-end/main/react-data-fetching/chimney.png" />
+            <img src="./src/img/chimney.png" alt="Factory with chimneys clipart" />
             <h1>Carbon Intensity Data</h1>
             <h3>from electricity generation in Great Britain</h3>
         </header>
