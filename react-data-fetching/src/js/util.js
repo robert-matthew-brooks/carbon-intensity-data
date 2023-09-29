1db9605ab978e3a2c09f7817b1abe90b02b63931
bob <robert.matthew.brooks@gmail.com>
2023-09-29 14:21:20 +0100

    small update - api header and dynamic font-size

--- react-data-fetching/src/js/util.js
+++ react-data-fetching/src/js/util.js
@@ -1,61 +1,107 @@
 function getDateDetails(daysBack) {
-    const date = new Date();
-    date.setDate(date.getDate() - daysBack);
-    const year = date.getFullYear();
-    let month = date.getMonth() + 1;
-    if (month < 10) month = '0' + month;
-    let day = date.getDate();
-    if (day < 10) day = '0' + day;
+  const date = new Date();
+  date.setDate(date.getDate() - daysBack);
+  const year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  if (month < 10) month = '0' + month;
+  let day = date.getDate();
+  if (day < 10) day = '0' + day;
 
-    const formattedDate = `
-        ${['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][date.getDay()]}
+  const formattedDate = `
+        ${
+          [
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+          ][date.getDay()]
+        }
         ${date.getDate()}
-        ${['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][date.getMonth()]}
+        ${
+          [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+          ][date.getMonth()]
+        }
         ${date.getFullYear()}
-    `.replace(/[ \n]+/g, ' ').slice(1, -1);
+    `
+    .replace(/[ \n]+/g, ' ')
+    .slice(1, -1);
 
-    const shortDate = `
+  const shortDate = `
         ${date.getDate()}
-        ${['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'][date.getMonth()]}
-    `.replace(/[ \n]+/g, ' ').slice(1, -1);
+        ${
+          [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sept',
+            'Oct',
+            'Nov',
+            'Dec',
+          ][date.getMonth()]
+        }
+    `
+    .replace(/[ \n]+/g, ' ')
+    .slice(1, -1);
 
-    return {
-        year,
-        month,
-        day,
-        formattedDate,
-        shortDate
-    }
+  return {
+    year,
+    month,
+    day,
+    formattedDate,
+    shortDate,
+  };
 }
 
 async function getApiData(daysBack) {
-    const from = getDateDetails(daysBack);
-    const to = getDateDetails(daysBack - 1);
+  const from = getDateDetails(daysBack);
+  const to = getDateDetails(daysBack - 1);
 
-    const dateFrom = `${from.year}-${from.month}-${from.day}T00:00Z`;
-    const dateTo = `${to.year}-${to.month}-${to.day}T00:00Z`;
+  const dateFrom = `${from.year}-${from.month}-${from.day}T00:00Z`;
+  const dateTo = `${to.year}-${to.month}-${to.day}T00:00Z`;
 
-    const response = await fetch(`https://api.carbonintensity.org.uk/intensity/${dateFrom}/${dateTo}`);
-    const { data } = await response.json();
-    return data;
+  const response = await fetch(
+    `https://api.carbonintensity.org.uk/intensity/${dateFrom}/${dateTo}`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+  const { data } = await response.json();
+  return data;
 }
 
 function showElement(id) {
-    const el = document.getElementById(id);
-    el.classList.remove('hide');
-    void el.offsetWidth;
-    el.classList.add('show');
+  const el = document.getElementById(id);
+  el.classList.remove('hide');
+  void el.offsetWidth;
+  el.classList.add('show');
 }
 
 function hideElement(id) {
-    const el = document.getElementById(id);
-    el.classList.remove('show');
-    el.classList.add('hide');
+  const el = document.getElementById(id);
+  el.classList.remove('show');
+  el.classList.add('hide');
 }
 
-export {
-    getDateDetails,
-    getApiData,
-    showElement,
-    hideElement
-};
+export { getDateDetails, getApiData, showElement, hideElement };
